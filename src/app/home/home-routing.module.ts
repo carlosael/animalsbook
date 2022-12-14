70e8bc04394cfab48a6cf357b9fe8f792c3d81a9
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-12-14 14:42:08 -0300

    feat: add login component

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,18 @@
+import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{
-  path: '',
-  component: HomeComponent
-}];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [{
+      path: '',
+      component: LoginComponent
+    }]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
