774606fe5f96db32624985326d843c3a86656ec7
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-12-19 16:58:46 -0300

    feat: add animals list service

---
+++ src/app/animals/animals.service.ts
@@ -0,0 +1,25 @@
+import { TokenService } from './../authentication/token.service';
+import { Animals } from './animal';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+const API = 'http://localhost:3000';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AnimalsService {
+  constructor(
+    private httpClient: HttpClient,
+    private tokenService: TokenService
+  ) {}
+
+  userList(userName: string): Observable<Animals> {
+    const token = this.tokenService.returnToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+    return this.httpClient.get<Animals>(`${API}/${userName}/photos`, {
+      headers,
+    });
+  }
+}
