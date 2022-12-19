88c2535603c2ceed9e32e855e6feed17e0962de9
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-12-19 10:08:08 -0300

    feat: add user authentication service

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,19 +1,37 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { UserService } from './user/user.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
   private url = 'http://localhost:3000/user/login';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(
+    private httpClient: HttpClient,
+    private userService: UserService
+  ) {}
 
-  authenticate(user: string, password: string): Observable<any> {
-    return this.httpClient.post(this.url, {
-      userName: user,
-      password: password
-    })
+  authenticate(user: string, password: string): Observable<HttpResponse<any>> {
+    return this.httpClient
+      .post(
+        this.url,
+        {
+          userName: user,
+          password: password,
+        },
+        {
+          observe: 'response',
+        }
+      )
+      .pipe(
+        tap((response) => {
+          const authToken = response.headers.get('x-access-token') ?? '';
+          this.userService.saveToken(authToken);
+        })
+      );
   }
 }
