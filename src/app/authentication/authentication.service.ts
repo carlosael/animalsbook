8b5664a8033f39ef57a6eac3990eb73e6bc95b53
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-12-19 17:09:08 -0300

    chore: add api url to enviroment

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,14 +1,17 @@
+import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UserService } from './user/user.service';
 import { tap } from 'rxjs/operators';
 
+const API = environment.apiURL;
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
-  private url = 'http://localhost:3000/user/login';
+  private url = API;
 
   constructor(
     private httpClient: HttpClient,
--- src/app/home/new-user/new-user.service.ts
+++ src/app/home/new-user/new-user.service.ts
@@ -1,18 +1,21 @@
+import { environment } from './../../../environments/environment';
 import { NewUser } from './new-user';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+const API = environment.apiURL;
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NewUserService {
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   signUp(newUser: NewUser) {
-    return this.httpClient.post('http://localhost:3000/user/signup', newUser);
+    return this.httpClient.post(`${API}/user/signup`, newUser);
   }
 
   verifyExistingUser(userName: string) {
-    return this.httpClient.get(`http://localhost:3000/user/exists/${userName}`)
+    return this.httpClient.get(`${API}/user/exists/${userName}`);
   }
 }
