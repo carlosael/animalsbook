d5f7a1b253e7b00c9f6ddebea056025956955bcb
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-12-20 13:52:18 -0300

    chore: using rxjs

--- src/app/animals/animals-list/animals-list.component.ts
+++ src/app/animals/animals-list/animals-list.component.ts
@@ -1,7 +1,9 @@
+import { switchMap } from 'rxjs/operators';
 import { AnimalsService } from './../animals.service';
 import { UserService } from './../../authentication/user/user.service';
 import { Component, OnInit } from '@angular/core';
 import { Animals } from '../animal';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-animals-list',
@@ -9,7 +11,7 @@ import { Animals } from '../animal';
   styleUrls: ['./animals-list.component.css'],
 })
 export class AnimalsListComponent implements OnInit {
-  animals!: Animals;
+  animals$!: Observable<Animals>;
 
   constructor(
     private userService: UserService,
@@ -17,11 +19,11 @@ export class AnimalsListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.userService.returnUser().subscribe((user) => {
-      const userName = user.name ?? '';
-      this.animalsSerice.userList(userName).subscribe((animals) => {
-        this.animals = animals;
-      });
-    });
+    this.animals$ = this.userService.returnUser().pipe(
+      switchMap((user) => {
+        const userName = user.name ?? '';
+        return this.animalsSerice.userList(userName);
+      })
+    );
   }
 }
