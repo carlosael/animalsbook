5bdaa8358dacfd19a7c0e917c5a06644485ebd96
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-12-20 14:05:16 -0300

    feat: add login guard to redirect to animals page if logged

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginGuard } from './authentication/login.guard';
 import { AuthenticationGuard } from './authentication/authentication.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -11,6 +12,7 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    canLoad: [LoginGuard],
   },
   {
     path: 'animals',
